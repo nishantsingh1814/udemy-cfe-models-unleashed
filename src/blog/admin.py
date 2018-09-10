a6bddee43f2d4209fbfd02acd0a41986329f5c47
Nishant <nishant@prakshep.com>
2018-09-10 14:40:15 +0530

    done

---
+++ src/blog/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'slug',
+        'content',
+        'publish',
+        'publish_date',
+        'active',
+        'updated',
+        'timestamp',
+        'get_age'
+    ]
+    readonly_fields = ['updated', 'timestamp', 'get_age']
+    def get_age(self, obj, *args, **kwargs):
+        return str(obj.age)
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
---
+++ src/blog/validators.py
@@ -0,0 +1,6 @@
+from django.core.exceptions import ValidationError
+
+def validate_author_email(value):
+    if not "@" in value:
+        raise ValidationError("Not a valid email")    
+    return value
